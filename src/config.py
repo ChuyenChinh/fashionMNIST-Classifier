ec9fe84a532d50c7698d75da48c52aa97fe2cf2b
Chuyen Chinh <chinhchuyen71@gmail.com>
2026-08-01 17:33:18 +0700

    fix config path

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
- 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.__file__))
-DATA_DIR = os.path.join(BASE_DIR,'data','raw')
-MODEL_DIR = os.path.join(BASE_DIR,'models')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'data', 'raw')
+MODEL_DIR = os.path.join(BASE_DIR, 'models')
 
 BATCH_SIZE = 300
 LEARNING_RATE = 0.001
